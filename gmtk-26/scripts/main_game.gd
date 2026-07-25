41e81f955e54304b313267792139e2c7cd0dcbf1
Lukas <gaindeez@gmail.com>
2026-07-25 14:06:16 +0200

    Shuffling background date rooms

--- gmtk-26/scripts/main_game.gd
+++ gmtk-26/scripts/main_game.gd
@@ -69,8 +69,16 @@ var transition_tween: Tween
 @onready var dev_cheat_btn: Button = $DevCheatBtn
 @onready var background_texture: TextureRect = $BackgroundTexture
 
+var room_textures: Array[Texture2D] = [
+	preload("res://assets/backgrounds/rooms/asylum_room_1.jpg"),
+	preload("res://assets/backgrounds/rooms/asylum_room_2.jpg"),
+	preload("res://assets/backgrounds/rooms/asylum_room_3.jpg"),
+	preload("res://assets/backgrounds/rooms/asylum_room_4.jpg")
+]
+var available_room_textures: Array[Texture2D] = []
 var bg_room_tex: Texture2D = preload("res://assets/backgrounds/asylum_room.jpg")
 var bg_hallway_tex: Texture2D = preload("res://assets/backgrounds/asylum_hallway.jpg")
+var active_date_room_tex: Texture2D = null
 
 var active_dialogue_balloon: Node = null
 var portrait_tween: Tween
@@ -119,6 +127,9 @@ func _input(event: InputEvent) -> void:
 
 func _start_new_game_session() -> void:
 	is_first_intro_transition = true
+	available_room_textures = room_textures.duplicate()
+	available_room_textures.shuffle()
+
 	var m_zombie = load("res://resources/monsters/zombie.tres")
 	var m_vampire = load("res://resources/monsters/vampire.tres")
 	var m_slime = load("res://resources/monsters/slime.tres")
@@ -162,7 +173,7 @@ func _show_panel(target_panel: Panel) -> void:
 	
 	if background_texture:
 		if target_panel == date_panel:
-			background_texture.texture = bg_room_tex
+			background_texture.texture = active_date_room_tex if active_date_room_tex else bg_room_tex
 			background_texture.visible = true
 		elif target_panel == prep_panel or target_panel == break_panel or target_panel == accusation_panel:
 			background_texture.texture = bg_hallway_tex
@@ -261,6 +272,13 @@ func _show_prep_phase() -> void:
 
 
 func _on_start_date_pressed() -> void:
+	if available_room_textures.is_empty():
+		available_room_textures = room_textures.duplicate()
+		available_room_textures.shuffle()
+
+	if not available_room_textures.is_empty():
+		active_date_room_tex = available_room_textures.pop_back()
+
 	var monster = GameManager.get_current_date_monster()
 	var m_name = monster.display_name if monster else "Candidate"
 	var m_species = monster.species if monster else ""
